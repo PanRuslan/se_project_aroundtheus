eaf734956962fe06a672a7a9b1af59407f57c898
Ruslan Ganiyev <ru.ganiyev@gmail.com>
2023-10-29 00:35:53 +0300

    Added interactivity to dialog window and cards render refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -25,13 +25,41 @@ const initialCards = [
   },
 ];
 
+function getCardElement(data) {
+  const cardList = document.querySelector(".cards");
+  const cardTemplate = cardList.querySelector("#card").content;
+  for (let card of data) {
+    const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+    cardItem.querySelector(".card__pic").src = card.link;
+    cardItem.querySelector(".card__pic").alt = card.name;
+    cardItem.querySelector(".card__title").textContent = card.name;
+    cardList.append(cardItem);
+  }
+}
+
+getCardElement(initialCards);
+
+const userName = document.querySelector(".user__name");
+const userStatus = document.querySelector(".user__status");
+
 const nameChangeModal = document.querySelector(".modal");
+const userFieldsModal = nameChangeModal.querySelectorAll(".modal__text-input");
 const nameChanger = document.querySelector(".user__name-changer");
 nameChanger.addEventListener("click", function () {
   nameChangeModal.classList.toggle("modal_opened");
+  userFieldsModal[0].value = userName.textContent;
+  userFieldsModal[1].value = userStatus.textContent;
 });
 
 const nameChangeModalCloser = nameChangeModal.querySelector(".modal__close");
 nameChangeModalCloser.addEventListener("click", function () {
   nameChangeModal.classList.remove("modal_opened");
+});
+
+const modalSubmitButton = nameChangeModal.querySelector(".modal__submit");
+modalSubmitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  nameChangeModal.classList.remove("modal_opened");
+  userName.textContent = userFieldsModal[0].value;
+  userStatus.textContent = userFieldsModal[1].value;
 });
